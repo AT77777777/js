1b8b78cbe2dc00c25abc5f6ff02ec6814c5307d1
AT77777777 <aat3337777@gmail.com>
2024-08-06 17:50:09 +0300

    Initial commit

---
+++ hw1/scripts/main.js
@@ -0,0 +1,74 @@
+//1
+let a = 'hello';
+
+let b = 'owu';
+
+let c = 'com';
+
+let d = 'ua';
+
+let e = 1;
+
+let f = 10;
+
+let g = -999;
+
+let h = 123;
+
+let i = 3.14;
+
+let j = 2.7;
+
+let k = 16;
+
+let l = true;
+
+let m = false;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+
+//2
+let firstName = 'Andrii';
+
+let middleName = 'Andriiovych';
+
+let lastName = 'Tkach';
+
+let person = `${firstName} ${middleName} ${lastName}`
+
+console.log(person);
+
+
+//3
+let a1 = 100;
+
+let b2 = '100';
+
+let c3 = true;
+
+console.log (typeof a1, b2, c3);
+
+
+//4
+// let firstName1 = prompt('First Name');
+//
+// let lastName1 = prompt('Last Name');
+//
+// let age = +prompt('Age');
+
+console.log(prompt('First Name'));
+console.log(prompt('Last Name'));
+console.log(+prompt('Age'));
